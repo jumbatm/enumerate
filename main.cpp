1a73f23328afea65c72f5679fb4de2420a3aa61b
Jonathan F <fungjys@gmail.com>
2018-08-17 11:39:45 +1000

    Run autoformatter, rename

--- main.cpp
+++ main.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 #include <vector>
+
 #include "enumerate.hh"
 
 int main()
 {
-    std::vector<int> v{ 1, 2, 3, 4};
+    std::vector<int> v{ 1, 2, 3, 4 };
 
     std::cout << "Enumerate over glvalue vector.\n";
 
-    for (auto&&[index, item] : jon::enumerate(v))
+    for (auto&& [index, item] : jon::enumerate(v))
     {
-        std::cout << index << " " << item  << "\n";
+        std::cout << index << " " << item << "\n";
     }
 
     std::cout << "Assign 3 to all values.\n";
 
-    for (auto&&[index, item] : jon::enumerate(v))
+    for (auto&& [index, item] : jon::enumerate(v))
     {
         item = 3;
         std::cout << index << " " << item << "\n";
     }
-
 }
