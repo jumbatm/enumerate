f13e077e040d8d9b38423a4c9d1c0ffc16f3270f
Jonathan F <fungjys@gmail.com>
2018-08-17 11:36:19 +1000

    Initial commit

---
+++ enumerate.hpp
@@ -0,0 +1,61 @@
+#ifndef ENUMERATE_H_INCLUDED
+#define ENUMERATE_H_INCLUDED
+
+#include <utility>
+#include <tuple>
+
+namespace jon
+{
+template <typename T>
+using collapse_t = T&;
+
+template <typename T>
+struct enumeratedIterator
+{
+    size_t index = 0;
+
+    T& m_container;
+
+    enumeratedIterator(T& container) : m_container(container)
+    {
+    }
+
+    size_t internalIndex = 0;
+
+    auto& begin() 
+    {
+        return *this;
+    }
+
+    size_t end()
+    {
+        return m_container.size();
+    }
+
+    enumeratedIterator<T>& operator++()
+    {
+        ++index;
+        return *this;
+    }
+
+    bool operator!=(size_t endValue)
+    {
+        return index != endValue;
+    }
+
+    auto operator*()
+    {
+        return std::tie(index, *(m_container.begin() + index));
+    }
+
+};
+
+template <typename T>
+auto enumerate(T&& container)
+{
+    return enumeratedIterator<T>(std::forward<T>(container));
+}
+
+}  // namespace std
+
+#endif
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	c++ main.cpp -Wall -Wextra -std=c++17 -o ./a -g
